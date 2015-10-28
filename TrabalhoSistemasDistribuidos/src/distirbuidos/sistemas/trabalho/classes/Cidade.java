af7f128f14e5449588553f3deee643bd97185ebd
FelipeLanzarin <lanzarin.felipe@gmail.com>
2015-10-28 18:52:12 -0200

    Classes de Principais Objetos(Cidade, Cep, Contato)

---
+++ TrabalhoSistemasDistribuidos/src/distirbuidos/sistemas/trabalho/classes/Cidade.java
@@ -0,0 +1,101 @@
+package distirbuidos.sistemas.trabalho.classes;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Cidade {
+
+    private int codigo;
+    private String nome;
+    private String estado;
+    private List<Cep> ceps;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + this.codigo;
+        hash = 89 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cidade other = (Cidade) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return  nome + " | " + estado;
+    }
+
+    public Cidade() {
+    }
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public List<Cep> getCeps() {
+        return ceps;
+    }
+
+    public void setCeps(List<Cep> ceps) {
+        this.ceps = ceps;
+    }
+
+}
