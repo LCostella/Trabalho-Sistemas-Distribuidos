89a02ce52521bbc52fbeba099c0e1c8c6e5c9f84
giovanirizzardi@gmail.com <c3techc2>
2015-10-31 19:23:59 -0200

    classe main
    classe main implantada

---
+++ TrabalhoSistemasDistribuidos/src/trabalho/sistemas/distribuidos/TrabalhoSistemasDistribuidos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho.sistemas.distribuidos;
+
+import distribuidos.sistemas.trabalho.telas.TelaPrincipal;
+
+/**
+ *
+ * @author Bola de Pelos
+ */
+public class TrabalhoSistemasDistribuidos {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        TelaPrincipal tp = new TelaPrincipal();
+        tp.setVisible(true);        
+    }
+    
+}
