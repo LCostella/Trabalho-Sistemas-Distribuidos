160876c88b5fda01a499476d69a91045ee12ffa0
LCostella <leo.costella@gmail.com>
2015-11-26 22:01:47 -0200

    Revert "Revert "ReceptorUDP(Novo)""
    This reverts commit fc0088931e50d366b70d19cd3135beed95f37f61.

---
+++ ReceptorUDPv.2/src/receptor/udp/RetornarCliente.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package receptor.udp;
+
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.List;
+
+/**
+ *
+ * @author felipe
+ */
+public class RetornarCliente {
+    
+    /**
+    * @param endereco  endereco de ip do receptor
+    * @param msg mensagem
+    */
+    public void responder(InetAddress endereco, String msg) throws Exception{
+        int porta = 2010;
+        byte data[] = new byte[150];//verificar se tem que mudar no cliente
+        data = msg.getBytes();
+        DatagramSocket soc = new DatagramSocket(porta);
+        DatagramPacket pac = new DatagramPacket(data, data.length, endereco, porta);
+        soc.send(pac);
+        soc.close();
+    }
+    
+    /**
+    * @param endereco  endereco de ip do receptor
+    * @param lista lista de mensagens
+    */
+    public void responder(InetAddress endereco, List<String> lista) throws Exception{
+        
+        for(String s : lista){
+            responder(endereco, s);
+        }
+        responder(endereco, "end");
+
+    }
+}
